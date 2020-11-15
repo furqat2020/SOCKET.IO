42c19463bd1a8c8dc02adb017cc5ba774e58211d
furqat2020 <65722945+furqat2020@users.noreply.github.com>
2020-11-15 21:43:47 +0500

    socket.io

---
+++ script.js
@@ -0,0 +1,38 @@
+const socket = io('http://localhost:3000')
+
+const messageContainer = document.getElementById('message-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+const name = prompt('Ismingiz?')
+appendMessage(`${name} Online`)
+socket.emit('new-user', name)
+
+socket.on('chat-message', data => {
+    appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+    appendMessage(`${name} ulandi. `)
+})
+
+messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    if(message != ''){
+        appendMessage(`You: ${message}`)
+        socket.emit('send-chat-message', message)
+        messageInput.value = ''        
+    }
+})
+
+function appendMessage(message){
+    const messageElement = document.createElement('li')
+
+    messageElement.innerText = message
+    messageContainer.append(messageElement)
+}
+
+var my_ul = document.getElementById('message-container')
+var my_li = my_ul.firstChild
+my_li.setAttribute('class', 'first_li')
